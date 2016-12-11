df08ca5488b1665d897954387498d255f5e190df
Vulliard Loan <lv3895@neuf.fr>
2016-12-11 14:53:24 +0900

    Added evidence computation

--- naiveBayes.py
+++ naiveBayes.py
@@ -13,7 +13,8 @@ Options:
 ################################### Imports ###################################
 from docopt import docopt
 import numpy as np
-import pickle
+import pandas as pd
+import pickle, random
 
 ################################## Functions ##################################
 
@@ -24,3 +25,23 @@ import pickle
 # Seed for reproductibility
 random.seed(1)
 
+# Data loading
+data = pickle.load(open("/home/koala/Documents/Scripts/naiveBayes/T.ft", "r"))
+nbFt = len(data[0]) - 1
+
+# Counting the probabilities for each column for each class
+evidence = [pd.DataFrame()]*nbFt
+for i in xrange(nbFt):
+	evidence[i] = pd.crosstab(index = data[:,-1], columns = data[:,i], margins=True)
+
+# e.g feature 1, proba of each class for the value 0
+print evidence[1][0]
+# e.g feature 2, proba of class 2 knowing value = 1
+print evidence[2][1][2]/float(evidence[2][1]["All"])
+
+# Counting the overall probabilities (prior)
+
+# Prediction
+# Multiplying the probabilities for each column of the input
+# Multiplying by the overall probabilities
+# Outputing the most likely
