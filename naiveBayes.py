bfef3e732d0c0adb93745f40aa74efec04f7967b
Vulliard Loan <lv3895@neuf.fr>
2016-12-11 15:54:54 +0900

    Added posterior probability computation and prediction

--- naiveBayes.py
+++ naiveBayes.py
@@ -27,25 +27,34 @@ random.seed(1)
 
 # Data loading
 data = pickle.load(open("/home/koala/Documents/Scripts/naiveBayes/T.ft", "r"))
+testData = np.array([[2,0,1,0,1,0,2,2],[0,1,2,1,2,1,0,1],[1,0,1,1,1,1,1,2]])
 nbFt = len(data[0]) - 1
+nbTr = len(testData)
 
 # Counting the probabilities for each column for each class
-evidence = [pd.DataFrame()]*nbFt
+likelihood = [pd.DataFrame()]*nbFt
 for i in xrange(nbFt):
-	evidence[i] = pd.crosstab(index = data[:,-1], columns = data[:,i], margins=True)
+	likelihood[i] = pd.crosstab(index = data[:,-1], columns = data[:,i], margins=True)
 
 # e.g feature 1, proba of each class for the value 0
-# evidence[1][0]
-# e.g feature 2, proba of class 2 knowing value = 1
-# evidence[2][1][2]/float(evidence[2][1]["All"])
+# likelihood[1][0]
+# e.g feature 2, proba of value = 1 knowing class = 2
+# likelihood[2][1][2]/float(likelihood[2]["All"][2])
 
 # Counting the overall probabilities (prior)
 prior = pd.crosstab(index = data[:,-1], columns="count", margins=True)["count"]
+nbClasses = len(prior)-1
 
 # e.g prior of class 1
-print prior[1]/float(prior["All"])
+# prior[1]/float(prior["All"])
 
 # Prediction
-# Multiplying the probabilities for each column of the input
-# Multiplying by the overall probabilities
-# Outputing the most likely
+prediction = [0]*nbTr
+for i in xrange(nbTr):
+	# Multiplying by the overall probabilities
+	probas = prior[:nbClasses]/float(prior[-1])
+	# Multiplying the probabilities for each column of the input
+	for j in xrange(nbFt):
+		probas = probas * likelihood[j][testData[i,j]][:-1]/likelihood[j]["All"][:-1]
+	# Outputing the most likely
+	prediction[i] = list(probas[probas == max(probas)].index)[0]
